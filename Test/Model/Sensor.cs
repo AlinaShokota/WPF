1f8bce23ed52c9fd345a5d70b9ac4214898e295c
Alina Shokota <alina.shokota159@gmail.com>
2021-04-18 23:42:30 +0200

    Created test wpf

---
+++ Test/Model/Sensor.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace Test.Model
+{
+    public class Sensor : INotifyPropertyChanged
+    {
+
+        private double actualTemp;
+        public double ActualTemp
+        {
+            get
+            {
+                return actualTemp;
+            }
+            set
+            {
+                actualTemp = value;
+                OnPropertyChanged();
+            }
+        }
+        private double goalTemp;
+        public double GoalTemp
+        {
+            get
+            {
+                return goalTemp;
+            }
+            set
+            {
+   
+                goalTemp = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Sensor()
+        {
+            //ActualTemp = measureTemp();
+            
+            double temp = measureTemp();
+            ActualTemp = Math.Round(temp, 2);
+
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+
+        private double measureTemp()
+        {
+            Random rnd = new Random();
+            double val = rnd.Next(18, 30);
+            return val;
+        }
+
+        public void changeTemp(double actual, double goal)
+        {
+            if (actual<goal)
+            {
+                ActualTemp += 0.1;
+            }
+            else if (actual>goal)
+            {
+                ActualTemp -= 0.1;
+            }
+            else
+            {
+
+            }
+        }
+
+    }
+}
+/*
+public class Sensor : INotifyPropertyChanged
+{
+
+    private bool heat;
+    public bool Heat
+    {
+        get
+        {
+            return heat;
+        }
+        set
+        {
+            heat = value;
+            OnPropertyChanged();
+        }
+    }
+    private double actualTemp;
+    public double ActualTemp
+    {
+        get
+        {
+            return actualTemp;
+        }
+        set
+        {
+            if (ActualTemp > GoalTemp)
+            {
+                heat = false;
+            }
+            else
+            {
+                heat = true;
+            }
+            actualTemp = value;
+            OnPropertyChanged();
+        }
+    }
+    private double goalTemp;
+    public double GoalTemp
+    {
+        get
+        {
+            return goalTemp;
+        }
+        set
+        {
+            if (GoalTemp > ActualTemp)
+            {
+                heat = true;
+            }
+            else
+            {
+                heat = false;
+            }
+            goalTemp = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public Sensor()
+    {
+        //ActualTemp = measureTemp();
+
+        double temp = measureTemp();
+        ActualTemp = Math.Round(temp, 2);
+
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+    public void OnPropertyChanged([CallerMemberName]string prop = "")
+    {
+        if (PropertyChanged != null)
+            PropertyChanged(this, new PropertyChangedEventArgs(prop));
+    }
+
+    private double measureTemp()
+    {
+        Random rnd = new Random();
+        double val = rnd.Next(18, 30);
+        return val;
+    }
+
+    public void changeTemp(double actual, double goal)
+    {
+        if (actual < goal)
+        {
+            ActualTemp += 0.1;
+        }
+        else if (actual > goal)
+        {
+            ActualTemp -= 0.1;
+        }
+        else
+        {
+
+        }
+    }
+
+}
+
+
+
+
+
+/*public class Sensor : INotifyPropertyChanged
+    {
+        private double currentTemp;
+
+        public DispatcherTimer _timer;
+
+        public double CurrentTemp
+        {
+            get
+            {
+                return this.currentTemp;
+            }
+            set
+            {
+                if (currentTemp == value)
+                    return;
+                currentTemp = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Sensor()
+        {
+            _timer = new DispatcherTimer(DispatcherPriority.Render);
+            _timer.Interval = TimeSpan.FromSeconds(3);
+            _timer.Tick += (sender, args) =>
+            {
+                double temp = CurrentTemp + 0.1;
+                CurrentTemp = Math.Round(temp, 2);
+
+            };
+            _timer.Start();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+
+    } */
---
+++ Test/ViewModel/TempViewModel.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Threading;
+using Test.Model;
+using Test.ViewModel.Action;
+
+namespace Test.ViewModel
+{
+    class TempViewModel : INotifyPropertyChanged
+    {
+        public DispatcherTimer _timer;
+        private double currentTemp;
+        public double CurrentTemp
+        {
+            get
+            {
+                return this.currentTemp;
+            }
+            set
+            {
+                if (currentTemp == value)
+                    return;
+                currentTemp = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private double goalTemp;
+        public double GoalTemp
+        {
+            get
+            {
+                return goalTemp;
+            }
+            set
+            {
+             
+                goalTemp = value;
+                CheckAndEnableButton();
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand SubmitButtonCommand { get; set; }
+
+        public TempViewModel()
+        {
+            CheckAndEnableButton();
+            Sensor sensor = new Sensor();
+            CurrentTemp = sensor.ActualTemp;
+            GoalTemp = CurrentTemp;
+
+            _timer = new DispatcherTimer(DispatcherPriority.Render);
+            _timer.Interval = TimeSpan.FromSeconds(3);
+            _timer.Tick += (sender, args) =>
+            {
+                sensor.changeTemp(CurrentTemp, GoalTemp);
+                double temp = sensor.ActualTemp;
+                CurrentTemp = Math.Round(temp, 2);
+
+            };
+            _timer.Start();
+            
+        }
+
+        public void CheckAndEnableButton()
+        {
+
+            bool isEnabled = false;
+             string result = string.Empty;
+             if (GoalTemp>17&&GoalTemp<28)
+             {
+                 isEnabled = true;
+                 result = string.Format("Goal temperature is set: \n{0}", GoalTemp);
+            }
+             else
+             {
+                 isEnabled = false;
+             }
+
+             SubmitButtonCommand = new RelayCommand((ob) => { return isEnabled; }, (ob) => { MessageBox.Show(result); });
+          
+            OnPropertyChanged("SubmitButtonCommand");
+
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+
+    /*class TempViewModel : INotifyPropertyChanged
+    {
+        public DispatcherTimer _timer;
+        private double currentTemp;
+        public double CurrentTemp
+        {
+            get
+            {
+                return this.currentTemp;
+            }
+            set
+            {
+                if (currentTemp == value)
+                    return;
+                currentTemp = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public TempViewModel()
+        {
+            Sensor sensor = new Sensor();
+            CurrentTemp = sensor.ActualTemp;
+
+            _timer = new DispatcherTimer(DispatcherPriority.Render);
+            _timer.Interval = TimeSpan.FromSeconds(3);
+            _timer.Tick += (sender, args) =>
+            {
+                sensor.changeTemp(CurrentTemp, 21);
+                double temp = sensor.ActualTemp;
+                CurrentTemp = Math.Round(temp, 2);
+
+            };
+            _timer.Start();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }*/
+}
